19a7d5f2582f715d6866eba9aaeac909c072afa1
Jerez <41114425+mateusjerez@users.noreply.github.com>
2024-11-19 18:51:42 -0300

    button add

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,15 @@
 export default function Home() {
   return (
     <div className="flex justify-center">
-      <div className="w-[800px] bg-blue-100 ">
-        <h1>Tasks List</h1>
-        <p>Here you can add, remove and mark tasks as done.</p>
+      <div className="flex flex-col w-[800px] bg-blue-100 ">
+        <div className="p-4">
+          <p className="flex font-bold text-xl justify-center pb-4">Tasks List</p>
+          <p>Aqui você pode adicionar, remover ou marcar as tarefas como concluídas.</p>
 
+        </div>
+        <div className="p-4">
+          <button className="flex w-24 justify-center border bg-blue-200 hover:bg-blue-300 border-slate-400">Add Task</button>
+        </div>
         
       </div>
     </div>
