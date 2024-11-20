66eb041072c3d2e89cfbaf30d592bd388af7ab96
Jerez <41114425+mateusjerez@users.noreply.github.com>
2024-11-20 00:57:54 -0300

    update addTask

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,11 @@ export default function Home() {
         {addTaskVisible && (
           <div>
             <div className="size-full fixed top-0 left-0 bg-black/10">
-              <div className="flex-col my-10">
+              <div className="flex-col size-[400px] relative my-10 mx-auto">
+                <button className="absolute top-3 right-4"
+                onClick={setAddTaskVisibleFalse}>
+                  X
+                </button>
                 <div className="flex justify-center">
                   <div className="w-[400px] bg-white p-4">
                     <p className="font-bold text-xl">Add Task</p>
@@ -58,8 +62,7 @@ export default function Home() {
                       </select>
                     </div>
                     <div className="flex justify-center mt-4">
-                      <button className="w-24 border bg-blue-200 hover:bg-blue-300 border-slate-400"
-                      onClick={setAddTaskVisibleFalse}>
+                      <button className="w-24 border bg-blue-200 hover:bg-blue-300 border-slate-400">
                         Add
                       </button>
                     </div>
